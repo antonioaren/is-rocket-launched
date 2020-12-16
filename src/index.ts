74d0f8a0a686824d43d21132c9de308be4fa6dc6
Pedro Arenas <antonioaren2@gmail.com>
2020-12-16 11:18:20 +0000

    hot-fixing/trying to keep Heroku alive

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,19 @@ import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import * as admin from 'firebase-admin';
 import botManager from './manager/bot.manager';
-
+import Axios from 'axios';
 
 // import apiRoutes from './routes';
 dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
+
+// To keep heroku wake up, I will include a petition every 30 mins.
+
+setInterval(async (_) => {
+  Axios.get('https://launchedrocket.herokuapp.com/')
+}, 1800000)
 
 admin.initializeApp({
   credential: admin.credential.applicationDefault(),
